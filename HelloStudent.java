1ea4be342c40dc45898f525db329affd5e225454
Harsh-kumar-sinha-427742 <sinhaharshkumar2001@gmail.com>
2025-05-07 20:06:03 +0530

    My java-2

--- HelloStudent.java
+++ HelloStudent.java
@@ -2,5 +2,6 @@ public class HelloStudent{
 	public static void main(String args[]){
 		String name="mahesh harsh";
 		System.out.println("Hello from "+name);
+		System.out.println("Hello Jenkins");
 	}
 }
